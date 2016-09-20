07bcd27839233bedb669aea5f7a575827e3f6954
infphilo <infphilo@gmail.com>
2016-09-20 16:45:20 -0400

    Changed python code format from windows to unix style, and added some codes

--- Nucleosome_positioning.py
+++ Nucleosome_positioning.py
@@ -1,24 +1,29 @@
-# Nucleosome_positioning By Sangwoo Park, September 2016
+#!/usr/bin/env python
+
+# Nucleosome_positioning By Sangwoo Park and Daehwan Kim, September 2016
 # Calculate probability of nucleosome positioning on given DNA sequence
 # Yeast genome data Based on Pubmed, Saccharomyces cerevisiae S288c (assembly R64)
 # Positioning data Based on Kristin, et al, Nature 2012
 # Algorithm Based on Segal, et al, Nature 2006
 
+import sys, os
+from argparse import ArgumentParser, FileType
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+
 # read out yeast genome data
-ygenome={}
-f = open("data/scerevisiae.fa", "rU")
-for line in f:
-    if '>' in line:
-        key=line.strip('>').strip('\n')
-        s=''
-    else:
-        s += line.rstrip()
-        ygenome[key] = s
-f.close()
+def read_genome(fname):
+    genome = {}
+    for line in open(fname, "rU"):
+        if '>' in line:
+            key=line.strip('>').strip('\n')
+            s=''
+        else:
+            s += line.rstrip()
+            genome[key] = s
+    return genome
 
 # get complementary sequence 
 def get_comp (seq):
@@ -33,46 +38,35 @@ def get_comp (seq):
         if base == 'G':
             new_seq += 'C'
     return new_seq
-
-# read out nucleosome positioning & sequence data
-NCP_pos=[]; NCP_seq=[]
-f=open("data/nature11142-s2.txt")
-for line in f:
-    if len(line.split()) ==4:
-        NCP_pos.append(line.split())
-for i in range(len(NCP_pos)):
-    key=NCP_pos[i][0]; pos=int(NCP_pos[i][1]); q=float(NCP_pos[i][3])
-    st=(pos-1)-73; en=(pos-1)+73
-    #st=(pos-1); en=(pos-1) + 146
-    if q > 0 and st >=0 and en <= len(ygenome[key])-1:
-        seq=ygenome[key][st:en+1]; #rev_comp_seq=get_comp(seq)[::-1]
-        NCP_seq.append(seq); #NCP_seq.append(rev_comp_seq)
-f.close()
-
+     
 # dinucleotide step analysis (A vs. G)
-def AG_freq (NCP_seq):
-    Afreq=np.zeros(146); Gfreq=np.zeros(146)
-    for seq in NCP_seq:
+def AG_freq (NCP_seq_list, nucleosome_dna_len):
+    Afreq=np.zeros(nucleosome_dna_len - 1); Gfreq=np.zeros(nucleosome_dna_len - 1)
+    for seq in NCP_seq_list:
         for i in range(len(seq)-1):
-            if seq[i:i+2] in ['AA','AT','TA','TT']:
-                Afreq[i] += 1.0/len(NCP_seq)
-            if seq[i:i+2] in ['CC','CG','GC','GG']:
-                Gfreq[i] += 1.0/len(NCP_seq)
+            dint = seq[i:i+2]
+            if dint in ['AA','AT','TA','TT']:
+                Afreq[i] += 1.0/len(NCP_seq_list)
+            elif dint in ['CC','CG','GC','GG']:
+                Gfreq[i] += 1.0/len(NCP_seq_list)
     return Afreq, Gfreq
 
 # dinucleotide step analysis (full)
-def din_freq (NCP_seq):
-    freq={'AA':np.zeros(146), 'TT':np.zeros(146), 'AC':np.zeros(146), 'GT':np.zeros(146),'AG':np.zeros(146), 'CT':np.zeros(146), 'AT':np.zeros(146), 'CA':np.zeros(146), 'TG':np.zeros(146), 'CC':np.zeros(146), 'GG':np.zeros(146), 'CG':np.zeros(146), 'GA':np.zeros(146), 'TC':np.zeros(146), 'GC':np.zeros(146), 'TA':np.zeros(146)}
-    for seq in NCP_seq:
+def din_freq (NCP_seq_list, nucleosome_dna_len):
+    freq = {}
+    for seq in NCP_seq_list:
         for i in range(len(seq)-1):
-            freq[seq[i:i+2]][i] += 1.0 / len(NCP_seq)
+            dint = seq[i:i+2]
+            if dint not in freq:
+                freq[dint] = np.zeros(nucleosome_dna_len)
+            freq[dint][i] += 1.0 / len(NCP_seq_list)
     return freq
 
 # get transition matrix of NCP positining
-def get_NCP_m (NCP_seq):    
-    freq=din_freq(NCP_seq); 
+def get_NCP_m (NCP_seq_list, nucleosome_dna_len):    
+    freq=din_freq(NCP_seq_list, nucleosome_dna_len);
     result=[]; base=["A", "T", "C", "G"]
-    for i in range(146):
+    for i in range(nucleosome_dna_len - 1):
         matrix=np.zeros([4,4])
         for j in range(4):
             norm=0.0; row=np.zeros(4)
@@ -83,11 +77,9 @@ def get_NCP_m (NCP_seq):
         result.append(matrix)
     return result
 
-tm=get_NCP_m(NCP_seq)
-
 # calculate probabilty of NCP positioning on given 147 bp sequence
-def get_NCP_prob(seq):
-    if len(seq) != 147: return None
+def get_NCP_prob(seq, nucleosome_dna_len):
+    if len(seq) != nucleosome_dna_len: return None
     global tm
     prob=0.25; dic={'A':0, 'T':1, 'C':2, 'G':3}
     for i in range(146):
@@ -96,49 +88,49 @@ def get_NCP_prob(seq):
     return prob
 
 # calculate the sum of weight-factor of given sub sequence (forward)
-def get_forward (seq, n):
+def get_forward (seq, n, nucleosome_dna_len):
     F=[1.0]
     for i in range(1,n+1):
-        if i <= 146:
+        if i < nucleosome_dna_len:
             F.append(F[-1]*1)
         else:
-            F.append(F[-1]*1 + F[-147]*get_NCP_prob(seq[i-147:(i-1)+1]))
+            F.append(F[-1]*1 + F[-nucleosome_dna_len]*get_NCP_prob(seq[i-nucleosome_dna_len:(i-1)+1]))
     return F
 
 # calculate the sum of weight-factor of given sub sequence (reverse)
-def get_reverse (seq, n):
+def get_reverse (seq, n, nucleosome_dna_len):
     R=[1.0]; N=len(seq)
     for i in range(1,n+1):
-        if i <= 146:
+        if i < nucleosome_dna_len:
             R.append(R[-1]*1)
         else:
-            R.append(R[-1]*1 + R[-147]*get_NCP_prob(seq[N-i:(N-i+146)+1]))
+            R.append(R[-1]*1 + R[-nucleosome_dna_len]*get_NCP_prob(seq[N-i:N-i+nucleosome_dna_len]))
     return R
 
 # calculate the probabilty of NCP would be on i th position of given sequence
-def NCPprob (seq, i):
-    if len(seq[i-1:]) < 147:
+def NCPprob (seq, i, nucleosome_dna_len):
+    if len(seq[i-1:]) < nucleosome_dna_len:
         return 0.0
-    return (get_forward(seq, i-1)[-1] * get_NCP_prob (seq[i-1:(i+145)+1]) * get_reverse(seq, len(seq)-(i+146))[-1])/ get_reverse(seq, len(seq))[-1]
+    return (get_forward(seq, i-1)[-1] * get_NCP_prob (seq[i-1:i+nucleosome_dna_len-1]) * get_reverse(seq, len(seq)-(i+nucleosome_dna_len-1))[-1]) / get_reverse(seq, len(seq))[-1]
 
 # get NCP positioning probabiliy profile for a long sequence
-def NCPprob_profile (seq):
+def NCPprob_profile (seq, nucleosome_dna_len):
     F=get_forward(seq, len(seq)); R=get_reverse(seq, len(seq))
     profile=[]
     for i in range(1, len(seq)+1):
-        if len(seq[i-1:]) < 147:
+        if len(seq[i-1:]) < nucleosome_dna_len:
             prob=0.0;
         else:
-            prob=(F[i-1] * get_NCP_prob (seq[i-1:(i+145)+1]) * R[len(seq)-(i+146)])/ R[-1]
+            prob=(F[i-1] * get_NCP_prob (seq[i-1:i+nucleosome_dna_len-1]) * R[len(seq)-(i+nucleosome_dna_len-1)]) / R[-1]
         profile.append(prob)
     return profile
 
 # get NCP occupancy profile of given sequence
-def NCPoccupancy (profile):
+def NCPoccupancy (profile, nucleosome_dna_len):
     result=[]
     for i in range(len(profile)):
         prob=0.0
-        for j in range(147):
+        for j in range(nucleosome_dna_len):
             pos=i-j
             if pos <0:
                 break
@@ -147,14 +139,65 @@ def NCPoccupancy (profile):
         result.append(prob)
     return result
 
-#freq=din_freq(NCP_seq)
-#newAfreq=freq['AA']+freq['AT']+freq['TA']+freq['TT']
-[Afreq,Gfreq]=AG_freq (NCP_seq)
-#print Afreq
-plt.figure(); plt.plot(Afreq); plt.show()
-#plt.figure(); plt.plot(Gfreq)
-#profile=NCPprob_profile (ygenome["chrII"])
-#occupancy=NCPoccupancy (profile)
-#plt.figure(); plt.plot(profile)
-#plt.figure(); plt.plot(occupancy)
-#print ygenome['chrI']
+
+def NCP(nucleosome_dna_len,
+        verbose):
+    # read out yeast genome data
+    ygenome = read_genome("data/scerevisiae.fa")
+
+    # read out nucleosome positioning & sequence data
+    NCP_list, NCP_seq_list = [], []
+    for line in open("data/nature11142-s2.txt"):
+        line = line.strip()
+        try:
+            chr, pos, score, noise= line.split()
+            pos, score, noise = int(pos) - 1, float(score), float(noise)
+            if noise <= 0.0:
+                continue
+            NCP_list.append([chr, pos, score / noise])
+        except ValueError:
+            continue
+
+    for NCP in NCP_list:
+        chr, pos, score = NCP
+        st = pos - nucleosome_dna_len / 2; en = pos + nucleosome_dna_len / 2
+        if st < 0 or en >= len(ygenome[chr]):
+            continue
+        seq = ygenome[chr][st:en+1]; #rev_comp_seq=get_comp(seq)[::-1]
+        NCP_seq_list.append(seq); #NCP_seq.append(rev_comp_seq)
+
+    tm = get_NCP_m(NCP_seq_list, nucleosome_dna_len)
+
+    #freq=din_freq(NCP_seq)
+    #newAfreq=freq['AA']+freq['AT']+freq['TA']+freq['TT']
+    Afreq, Gfreq = AG_freq(NCP_seq_list, nucleosome_dna_len)
+    #print Afreq
+    plt.figure(); plt.plot(Afreq); plt.show()
+    #plt.figure(); plt.plot(Gfreq)
+    #profile=NCPprob_profile (ygenome["chrII"])
+    #occupancy=NCPoccupancy (profile)
+    #plt.figure(); plt.plot(profile)
+    #plt.figure(); plt.plot(occupancy)
+    #print ygenome['chrI']
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(
+        description='Nucleosome center positioning')
+    parser.add_argument('--nucleosome-dna-len',
+                        dest='nucleosome_dna_len',
+                        type=int,
+                        default=147,
+                        help='Flanking sequence length (both sides including the center, default: 147')
+    parser.add_argument('-v', '--verbose',
+                        dest='verbose',
+                        action='store_true',
+                        help='also print some statistics to stderr')
+
+    args = parser.parse_args()
+    if args.nucleosome_dna_len % 2 == 0:
+        print >> sys.stderr, "Error: please use an odd number for --nucleosome-dna-len, perhaps %d instead of %d" % (args.nucleosome_dna_len + 1, args.nucleosome_dna_len)
+        sys.exit(1)
+
+    NCP(args.nucleosome_dna_len,
+        args.verbose)
