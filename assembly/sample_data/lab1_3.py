ee8a51234164a64d6ca41ff7fdda871574cc5ee1
Sangwoo <sparkly205@gmail.com>
2016-11-20 22:07:01 -0500

    a

--- assembly/sample_data/lab1_3.py
+++ assembly/sample_data/lab1_3.py
@@ -1,6 +1,18 @@
 
+# open fasta file
+def fasta_input (filename):
+    seq_reads={}
+    for line in open(filename):
+        if line[0] == '>':
+            key=int(line[1:-1])
+            seq_reads[key]=''
+        else:
+            seq_reads[key] += line.strip()
+    return seq_reads
+seq_info=fasta_input("sample.fasta")
+
 # open unitig file
-def open_file (file_name):
+def unis_input (file_name):
     unis_dic={}
     for line in open(file_name):
         linelist=line.strip().split()
@@ -11,4 +23,34 @@ def open_file (file_name):
             num,ori,pos=linelist
             unis_dic[uid].append([int(num),ori,int(pos)])
     return unis_dic
-unis_dic=open_file("sample.unis")
+unis_info=unis_input("sample.unis")
+
+# make complementary sequence
+def comp_seq (seq):
+    comp_base={'a':'t','t':'a','c':'g','g':'c'}
+    comp_seq=''
+    for base in seq:
+        comp_seq += comp_base[base]
+    return comp_seq[::-1]
+                            
+# generate unitig sequence
+def unis_syn (seq_info, unis_info):
+    unis_seq={}
+    for uid in unis_info:
+        seq=''
+        i=0; n,o,p = unis_info[uid][i]
+        while i < len(unis_info[uid]):
+            target=seq_info[n]
+            if o =='R':
+                target=comp_seq(target)
+            i+=1;
+            if i==len(unis_info[uid]):
+                np=len(target)
+            else:
+                nn, no, np = unis_info[uid][i]
+            seq += target[0:np]
+            n,o,p = nn,no,np
+        unis_seq[uid]=seq
+    return unis_seq
+unis_seq=unis_syn (seq_info, unis_info)
+
